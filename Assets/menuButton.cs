da08813607a9aefe0924311034c7009f200d8faf
fdavies93 <fdavies93@gmail.com>
2016-01-31 16:43:00 +0000

    removed standard assets, made slight changes

---
+++ Assets/menuButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuButton : MonoBehaviour {
+
+    GameObject shopPane;
+
+	// Use this for initialization
+	void Start () {
+        shopPane = GameObject.Find("MenuPanel");
+        shopPane.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        if(shopPane.activeInHierarchy)
+        {
+            shopPane.SetActive(false);
+        }
+        else
+        {
+            shopPane.SetActive(true);
+        }
+    }
+}
