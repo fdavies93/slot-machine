bbf774d9d6ca9bc9f6aab1f31d2e5d8a8efb2109
fdavies93 <fdavies93@gmail.com>
2016-02-05 19:02:28 +0000

    the best update

--- Assets/scripts/MinusBet.cs
+++ Assets/scripts/MinusBet.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class MinusBet : MonoBehaviour {
 
+    private pullLever script;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+        script = GameObject.Find("lever").GetComponent<pullLever>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public void BetMinus()
+    {
+        script.curBet -= 1;
+    }
 }
